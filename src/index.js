a044ef20db2269dd3c6034742660baae3e49e08e
Azim <azimarynaliev@icloud.com>
2025-02-04 10:36:53 -0600

    mongodb pairing

--- src/index.js
+++ src/index.js
@@ -17,3 +17,28 @@ app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
   V1SwaggerDocs(app, PORT);
 });
+
+const { MongoClient } = require("mongodb");
+
+// Replace the uri string with your connection string.
+const uri =
+  "mongodb+srv://moldalievazimbek:<67a23ab9b2cc0e38c155c94f>@cluster1.qgryf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    const database = client.db("sample_mflix");
+    const movies = database.collection("movies");
+
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: "Back to the Future" };
+    const movie = await movies.findOne(query);
+
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
